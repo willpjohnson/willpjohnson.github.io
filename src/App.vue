abf27eceb388e3d9c39827813602a67dec309f8e
Will Johnson <will.p.johnson@gmail.com>
2023-03-18 22:53:04 -0400

    use pug

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<template lang="pug">
+nav
+  router-link(
+    to="/"
+  ) Home
+  router-link(
+    to="/about"
+  ) About
+router-view
 </template>
 
 <style>
