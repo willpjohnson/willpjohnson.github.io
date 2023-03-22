3d998be07f5d242b5c8b3209b838e9870804b1a0
Will Johnson <will.p.johnson@gmail.com>
2023-03-21 23:41:52 -0400

    detect closest section

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -4,9 +4,10 @@
     .link(
       v-for="tab in tabs"
       :key="tab.name"
+      :class="linkClass(tab.ref)"
       @click="scrollTo(tab.ref)"
     ) {{  tab.name  }}
-  .parent-container
+  .parent-container(ref="container")
     SoundCloud(ref="soundcloud")
     .border
     LiveTheater(ref="theater")
@@ -28,20 +29,57 @@ export default {
     ScenesIWishIScored,
   },
 
-  computed: {
-    tabs() {
-      return [
+  data() {
+    return {
+      closestRef: null,
+      tabs: [
         { name: 'Soundcloud', ref: 'soundcloud' },
         { name: 'Theater', ref: 'theater' },
         { name: 'Scenes I Wish I Scored', ref: 'scenes' },
-      ];
-    },
+      ],
+    };
+  },
+
+  mounted() {
+    const container = this.$refs.container;
+    container.onscroll = this.onScroll;
+    this.tabs.forEach((tab) => {
+      const top = this.$refs[tab.ref].$el.offsetTop;
+      const height = this.$refs[tab.ref].$el.offsetHeight;
+      const middle = top + (height / 2);
+      tab.position = middle;
+    });
+    this.closestRef = this.tabs[0].ref;
   },
 
   methods: {
     scrollTo(ref) {
       const el = this.$refs[ref].$el;
       el.scrollIntoView();
+    },
+
+    onScroll() {
+      const container = this.$refs.container;
+      const containerHeight = container.offsetHeight;
+      const scrollTop = container.scrollTop;
+      const middleOfScreen = scrollTop + (containerHeight / 2);
+
+      let closestTab = { ref: null, dist: -1 };
+      this.tabs.forEach((tab) => {
+        const dist = Math.abs(middleOfScreen - tab.position);
+        if (closestTab.dist < 0) {
+          closestTab = { ref: tab.ref, dist };
+        } else {
+          if (closestTab.dist > dist) closestTab = { ref: tab.ref, dist };
+        }
+      });
+      this.closestRef = closestTab.ref;
+    },
+
+    linkClass(ref) {
+      if (this.closestRef === ref) {
+        return 'selected';
+      }
     },
   },
 }
@@ -77,6 +115,7 @@ export default {
     height: 100%;
     max-height: 100%;
     overflow-y: scroll;
+    overflow-x: hidden;
     scroll-behavior: smooth;
   }
 
